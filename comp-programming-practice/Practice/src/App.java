8da26232d6509289d6da20e7dcb5c97411f58eda
Cindy <zhaoc124@gmail.com>
2022-10-08 22:02:27 -0400

    hashmap method

--- comp-programming-practice/Practice/src/App.java
+++ comp-programming-practice/Practice/src/App.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -6,7 +8,25 @@ public class App {
 }
 
     // Leetcode 70
-    public static int LC70(int n){
+    public static int recursionMethod(int n) {
+        if (n == 1) // if only one stair left
+        {
+            return 1;
+        }
+
+        if (n == 2) // if only two stairs left
+        {
+            return 2;
+        }
+
+        return recursionMethod(n - 1) + recursionMethod(n - 2); // if staires > 3, return if first step is 1 and if
+                                                                // first step is 2
+    }
+
+    public static int hashmapMethod(int n)
+    {
+        HashMap<Integer, Integer> climbStairs = new HashMap<Integer, Integer>();
+
         if (n == 1) //if only one stair left
         {
             return 1;
@@ -17,5 +37,14 @@ public class App {
             return 2;
         }
 
-        return LC70(n-1) + LC70(n-2); //if staires > 3, return if first step is 1 and if first step is 2
+        if (n >= 3 && null != climbStairs.get(n))
+        {
+            return climbStairs.get(n);
+        }
+
+        else
+        {
+            climbStairs.put(n, hashmapMethod(n-1) + hashmapMethod(n-2));
+            //return hashmapMethod(n-1) + hashmapMethod(n-2);
+        }
     }
